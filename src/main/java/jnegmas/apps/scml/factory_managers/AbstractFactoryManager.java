8de56211158eeb16b10a2fa9ae518f769c2496df
Yasser Mohammad <yasserfarouk@gmail.com>
2019-03-11 19:08:55 +0900

    adding base interface files

--- src/main/java/jnegmas/apps/scml/factory_managers/AbstractFactoryManager.java
+++ src/main/java/jnegmas/apps/scml/factory_managers/AbstractFactoryManager.java
@@ -5,6 +5,7 @@ import jnegmas.apps.scml.awi.PySCMLAWI;
 import jnegmas.apps.scml.common.*;
 import jnegmas.apps.scml.common.Process;
 import jnegmas.apps.scml.simulators.PyFactorySimulator;
+import jnegmas.situated.PyAgentWorldInterface;
 
 import java.util.ArrayList;
 import java.util.HashMap;
@@ -26,22 +27,24 @@ public abstract class AbstractFactoryManager extends SCMLAgent implements Factor
     ArrayList<Product> products;
     ArrayList<Process> processes;
     ArrayList<Integer> interesting_products;
+    String name;
 
-    public boolean test(HashMap<String, Object> map){
+    public boolean test(HashMap<String, Object> map) {
         CFP cfp = new CFP(map);
         System.out.format("product %d", cfp.product);
         return true;
     }
 
-    public void setAWI(PySCMLAWI value){
+    public void setAWI(PySCMLAWI value) {
+
         this.awi = value;
     }
 
-    public PySCMLAWI getAWI(){
+    public PySCMLAWI getAWI() {
         return awi;
     }
 
-    public void fromMap(HashMap<String, Object> dict){
+    public void fromMap(HashMap<String, Object> dict) {
         awi = (PySCMLAWI) dict.get("awi");
         simulator = (PyFactorySimulator) dict.get("simulator");
         transportation_delay = (int) dict.get("transportation_delay");
@@ -50,16 +53,17 @@ public abstract class AbstractFactoryManager extends SCMLAgent implements Factor
         simulator_type = (String) dict.get("simulator_type");
         immediate_negotiations = (boolean) dict.get("immediate_negotiations");
         negotiation_speed_multiple = (int) dict.get("negotiation_speed_multiple");
-        line_profiles = (HashMap<Integer,ManufacturingProfileCompiled>) dict.get("line_profiles");
-        process_profiles = (HashMap<Integer,ManufacturingProfileCompiled>) dict.get("process_profiles");
-        producing = (HashMap<Integer,ArrayList<ProductManufacturingInfo>>) dict.get("producing");
-        consuming = (HashMap<Integer,ArrayList<ProductManufacturingInfo>>) dict.get("consuming");
+        line_profiles = (HashMap<Integer, ManufacturingProfileCompiled>) dict.get("line_profiles");
+        process_profiles = (HashMap<Integer, ManufacturingProfileCompiled>) dict.get("process_profiles");
+        producing = (HashMap<Integer, ArrayList<ProductManufacturingInfo>>) dict.get("producing");
+        consuming = (HashMap<Integer, ArrayList<ProductManufacturingInfo>>) dict.get("consuming");
         compiled_profiles = (ArrayList<ManufacturingProfileCompiled>) dict.get("compiled_profiles");
         products = (ArrayList<Product>) dict.get("products");
         processes = (ArrayList<Process>) dict.get("processes");
         interesting_products = (ArrayList<Integer>) dict.get("interesting_products");
     }
-    public HashMap<String, Object> toMap(){
+
+    public HashMap<String, Object> toMap() {
         HashMap<String, Object> map = new HashMap<>();
         map.put("awi", awi);
         map.put("simulator", simulator);
@@ -78,6 +82,11 @@ public abstract class AbstractFactoryManager extends SCMLAgent implements Factor
         map.put("processes", processes);
         map.put("interesting_products", interesting_products);
         return map;
+    }
+
+    public void on_construction(String name, PyFactorySimulator simulator) {
+        this.name = name;
+        this.simulator = simulator;
     }
 
 }
